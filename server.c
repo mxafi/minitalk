ef49c5c360ca7d73c9456ecd66e0b39583ef0244
Markus Laaksonen <malaakso@student.hive.fi>
2023-02-12 20:01:48 +0200

    optimize signal handling

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 13:11:25 by malaakso          #+#    #+#             */
-/*   Updated: 2023/02/12 17:20:58 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/02/12 20:00:38 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,8 @@ void	got_data(int sig)
 	static unsigned char	c;
 	static int				i;
 
-	if (sig == SIGUSR1)
-	{
-		signal(SIGUSR1, got_data);
-	}
-	else
-	{
-		signal(SIGUSR2, got_data);
+	if (sig == SIGUSR2)
 		c |= (1 << i);
-	}
 	i++;
 	if (i == 8)
 	{
@@ -40,11 +33,12 @@ int	main(void)
 	pid_t	server_pid;
 
 	server_pid = getpid();
+	signal(SIGUSR1, got_data);
+	signal(SIGUSR2, got_data);
 	ft_printf("Server PID: %i\n", server_pid);
 	while (1)
 	{
-		signal(SIGUSR1, got_data);
-		signal(SIGUSR2, got_data);
+		usleep(1);
 	}
 	return (0);
 }
